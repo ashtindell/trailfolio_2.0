991ca14d729d7d15376946a275da107fd324717d
Ashley Tindell <tindell.ea@gmail.com>
2016-08-29 20:53:55 -0600

    update flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+    def flash_message type, text
+       flash[type] ||= []
+       flash[type] << text
+    end
+    
+    def render_flash
+       flash_array = []
+       flash.each do |type, messages|
+          if messages.is_a?(String)
+             flash_array << render(partial: 'shared/flash',
+             locals: { :type => type, :message => messages })
+          else
+            messages.each do |m|
+              flash_array << render(partial: 'shared/flash',
+              locals: { :type => type, :message => m }) unless m.blank?
+            end
+          end
+       end
+       flash_array.join('').html_safe
+    end
+
 end
