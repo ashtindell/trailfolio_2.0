77eee7e500e704de52c34c598b9c4955262f8ff3
Ashley Tindell <tindell.ea@gmail.com>
2016-08-29 21:22:14 -0600

    add more fields to user

---
+++ db/migrate/20160830031432_add_fields_to_user.rb
@@ -0,0 +1,13 @@
+class AddFieldsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :street_address, :string
+    add_column :users, :street_address_2, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :zip_code, :string
+    add_column :users, :latitude, :float
+    add_column :users, :longitude, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601222626) do
+ActiveRecord::Schema.define(version: 20160830031432) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20160601222626) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "street_address"
+    t.string   "street_address_2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip_code"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
